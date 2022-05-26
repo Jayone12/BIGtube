de2892d4c4ee3c80987239872fc0436035dfed2b
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-05-26 20:48:29 +0900

    feat: Create routers and view engine setting
    configure router using middleware and set view engine pug

--- src/server.js
+++ src/server.js
@@ -1,12 +1,24 @@
 import express from "express";
+import morgan from "morgan";
+import rootRouter from "./routers/rootRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-  console.log(`서버가 활성화 되었습니다., http://localhost/${PORT}`);
+// middleware
+const logger = morgan("dev");
+app.use(logger);
+
+app.use("/", rootRouter);
+app.use("/user", userRouter);
+app.use("/videos", videoRouter);
+
+app.listen(PORT, () => {
+  console.log(`서버가 활성화 되었습니다. http://localhost:${PORT}`);
 });
