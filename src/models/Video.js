e50b5d0dc0fa47b4266a2e68769b8fc5dec9eb6c
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-11 12:42:03 +0900

    fix: fix the video model options

--- src/models/Video.js
+++ src/models/Video.js
@@ -2,13 +2,13 @@ import mongoose from "mongoose";
 
 // schema 생성
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  createdAt: Date,
-  hashtags: [{ type: String }],
+  title: { type: String, required: true },
+  description: { type: String, required: true, maxLength: 80 },
+  createdAt: { type: Date, required: true, default: Date.now, minLength: 20 },
+  hashtags: [{ type: String, trim: true }],
   meta: {
-    views: Number,
-    rating: Number,
+    views: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
   },
 });
 
