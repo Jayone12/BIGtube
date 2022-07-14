818850f9b01a618959e95a2cf08340ec160ee5fe
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-14 09:28:57 +0900

    feat: Create search controller

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { trending } from "../controllers/VideoControllers";
+import { trending, search } from "../controllers/VideoControllers";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", trending);
+rootRouter.get("/search", search);
 
 export default rootRouter;
