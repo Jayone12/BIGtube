0dc174437eecf2c097459420796a34801e160047
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-14 16:25:35 +0900

    fix: Renamed from loggedUser to loggedInUser

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,6 @@
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "Bigtube";
   res.locals.loggedIn = Boolean(req.session.loggedIn);
-  res.locals.loggedUser = req.session.user;
+  res.locals.loggedInUser = req.session.user;
   next();
 };
