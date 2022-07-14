f5047de31ae4a3939cdb1553ecb806d491df6999
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-14 11:26:15 +0900

    feat: Add 'join' router and 'login' router setting

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { getJoin, postJoin, login } from "../controllers/userControllers";
 import { trending, search } from "../controllers/VideoControllers";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", trending);
+rootRouter.route("/join").get(getJoin).post(postJoin);
+rootRouter.get("/login", login);
 rootRouter.get("/search", search);
 
 export default rootRouter;
