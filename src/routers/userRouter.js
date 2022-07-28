293973ae4b7932d40da6ef827cb001b2c9026c3a
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-28 17:14:13 +0900

    feat: Create github login router

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,6 +1,12 @@
 import express from "express";
-import {} from "../controllers/userControllers";
+import {
+  finishGithubLogin,
+  startGithubLogin,
+} from "../controllers/userControllers";
 
 const userRouter = express.Router();
+
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 
 export default userRouter;
