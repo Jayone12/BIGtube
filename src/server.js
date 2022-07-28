cb2fbfb14993081f370b45ffc8cf78d85c1d1ee2
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-28 17:11:52 +0900

    fix: change 'user' route name 'users'

--- src/server.js
+++ src/server.js
@@ -25,7 +25,7 @@ app.use(
 
 app.use(localsMiddleware);
 app.use("/", rootRouter);
-app.use("/user", userRouter);
+app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
 export default app;
