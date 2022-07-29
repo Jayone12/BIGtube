de01d07c14e4026509f6041cc2bacacce900a550
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-29 13:29:36 +0900

    feat: add protector middleware to video router

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,13 +7,25 @@ import {
   postUpload,
   deleteVideo,
 } from "../controllers/VideoControllers";
+import { protectorMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
 // 몽고 db에서 자동 생성되는 ID 는 16진수로 0~9 a~f까지 24자로 이루어져 있다.
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter
+  .route("/:id([0-9a-f]{24})/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter
+  .route("/:id([0-9a-f]{24})/delete")
+  .all(protectorMiddleware)
+  .get(deleteVideo);
+videoRouter
+  .route("/upload")
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(postUpload);
 
 export default videoRouter;
