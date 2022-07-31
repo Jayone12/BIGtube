0b02f27a5476dbe1a52f7fc3cb1c884e5493c7e1
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-31 17:08:38 +0900

    feat: Find videos matching user ID

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,6 +1,7 @@
 import fetch from "node-fetch";
 import User from "../models/User";
 import bcrypt from "bcrypt";
+import Video from "../models/Video";
 
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Create Account" });
@@ -227,5 +228,9 @@ export const postChangePassword = async (req, res) => {
 export const userProfile = async (req, res) => {
   const { id } = req.params;
   const user = await User.findById(id);
-  return res.render("userProfile", { pageTitle: user.name, user });
+  if (!user) {
+    return res.status(404).render("404", { pageTitle: "User not found." });
+  }
+  const videos = await Video.find({ owner: user._id });
+  return res.render("userProfile", { pageTitle: user.name, user, videos });
 };
