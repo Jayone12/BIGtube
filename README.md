9cd081c665085b41727d94241630faf060bb467c
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-08-01 14:21:34 +0900

    style: Create pages styles

---
+++ README.md
@@ -0,0 +1,14 @@
+localhost:3000/ -> home
+
+- rootRouter
+  /search -> 검색
+  /join -> 회원가입
+  /login -> 로그인
+
+- videoRouter
+  /video/watch -> 영상 보는 페이지
+  /video/upload -> 영상 업로드
+
+- userRouter
+  /user/edit -> 회원 정보 변경
+  /user/profile -> 회원 정보
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  // 변환할 파일 위치
+  entry: {
+    main: "./src/client/js/main.js",
+    videoPlayer: "./src/client/js/videoPlayer.js",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
+  mode: "development",
+  watch: true,
+  output: {
+    // 변환 후 저장할 파일명
+    filename: "js/[name].js",
+    // 변환 후 저장 위치
+    path: path.resolve(__dirname, "assets"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
