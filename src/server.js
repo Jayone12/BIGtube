e3ae5af72ea2dc0581cf26f4f4cc4ac9f97728d0
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-08-03 19:46:31 +0900

    feat: Add express json middleware

--- src/server.js
+++ src/server.js
@@ -15,6 +15,7 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(logger);
 app.use(
   session({
