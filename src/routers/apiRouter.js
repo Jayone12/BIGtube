c029412a6d3ceed750abb0e76fa01d3d39b95c9f
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-08-04 11:09:50 +0900

    feat: Create comment delete router

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { registerView, createComment } from "../controllers/VideoControllers";
+import {
+  registerView,
+  createComment,
+  removeComment,
+} from "../controllers/VideoControllers";
 
 const apiRouter = express.Router();
 
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
 apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment);
+apiRouter.delete("/comment/:id([0-9a-f]{24})/delete", removeComment);
 
 export default apiRouter;
